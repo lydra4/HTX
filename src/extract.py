0606b826d3c5666814981dcc6a5810e1c52036f2
Song Yi <lydra4@gmail.com>
2025-11-22 04:29:08 +0000

    feat: entry script to load config file

---
+++ src/extract.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+import hydra
+from omegaconf import DictConfig
+
+from utils.general_utils import setup_logging
+
+
+@hydra.main(version_base=None, config_name="../config", config_path="extraction.yaml")
+def main(cfg: DictConfig):
+    logger = logging.getLogger(__name__)
+    setup_logging(
+        logging_config_path=os.path.join(
+            hydra.utils.get_original_cwd(), "config", "logging.yaml"
+        )
+    )
+    logger.info("Setting up logging configuration.")
